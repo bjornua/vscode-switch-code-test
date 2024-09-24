3f3fb6d82efab16ccae08b05f130cd56d47d9054
Martin Slota <martin.slota@workday.com>
2024-09-24 09:58:29 +0200

    Avoid negation in if condition

--- src/extension.ts
+++ src/extension.ts
@@ -103,17 +103,17 @@ async function openOrCreate(root: string, relPathCandidates: string[]) {
   const existingPathPair = pathPairs.find(({ absPath }) =>
     fs.existsSync(absPath)
   );
-  if (existingPathPair !== undefined) {
-    const doc = await vscode.workspace.openTextDocument(
-      existingPathPair.absPath
-    );
-    await vscode.window.showTextDocument(doc);
-  } else {
+  if (existingPathPair === undefined) {
     const { relPath, absPath } = pathPairs[0];
     if (!(await promptForFileCreation(relPath))) {
       return;
     }
     await createFile(absPath);
+  } else {
+    const doc = await vscode.workspace.openTextDocument(
+      existingPathPair.absPath
+    );
+    await vscode.window.showTextDocument(doc);
   }
 }
 
